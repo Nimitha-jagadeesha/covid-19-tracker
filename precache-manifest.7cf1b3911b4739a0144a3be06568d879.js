aece44b11aa785bc0c4a6daf71e4a6017c893c38
Nimitha J <nimitha1jagadeesha@gmail.com>
2020-06-06 11:27:34 +0530

    Updates

---
+++ precache-manifest.7cf1b3911b4739a0144a3be06568d879.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea999957489ad69c32e4b0cb17b6cc8",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "d46448b2b36acc5fb60e",
+    "url": "/covid-19-tracker/static/css/main.3d27b6ec.chunk.css"
+  },
+  {
+    "revision": "f0211fee165c7979d1f0",
+    "url": "/covid-19-tracker/static/js/2.956d2817.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.956d2817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46448b2b36acc5fb60e",
+    "url": "/covid-19-tracker/static/js/main.2490e0b6.chunk.js"
+  },
+  {
+    "revision": "12206515d32e06966cfa",
+    "url": "/covid-19-tracker/static/js/runtime-main.38dfe150.js"
+  },
+  {
+    "revision": "f3e6fbe5624e22a20026e2c7323c6263",
+    "url": "/covid-19-tracker/static/media/covid.f3e6fbe5.png"
+  }
+]);
